8f0c5a83cca4e2933b1bcb0b40d826925bd52d93
Sergio <sergio.eneide@protonmail.com>
2021-06-19 16:59:17 +0100

    src/

--- tests/pet_shop_test.py
+++ tests/pet_shop_test.py
@@ -78,8 +78,8 @@ class TestPetShop(unittest.TestCase):
     def test_pet_shop_name(self):
         name = get_pet_shop_name(self.cc_pet_shop)
         self.assertEqual("Camelot of Pets", name)
+ 
 
-    @unittest.skip("delete this line to run the test")
     def test_total_cash(self):
         sum = get_total_cash(self.cc_pet_shop)
         self.assertEqual(1000, sum)
