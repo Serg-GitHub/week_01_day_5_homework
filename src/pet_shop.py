1f4c52effbd345fbe8d8aa5d5bc6fe98a8fb00a6
Sergio <sergio.eneide@protonmail.com>
2021-06-20 19:51:38 +0100

    src

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -1,41 +1,63 @@
 # WRITE YOUR FUNCTIONS HERE
+
 def get_pet_shop_name(pet_shop):
     return pet_shop["name"]
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
 
-def add_or_remove_cash(pet_shop, cash_amount):
-    pet_shop["admin"]["total_cash"]= (pet_shop["admin"]["total_cash"]+cash_amount)
-
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
 
-def increase_pets_sold(pet_shop, pet_sales):
-     pet_shop["admin"]["pets_sold"] = (pet_shop["admin"]["pets_sold"]+pet_sales)
+def increase_pets_sold(pet_shop, count):
+    pet_shop["admin"]["pets_sold"] += count
 
 def get_stock_count(pet_shop):
-   return len(pet_shop ["pets"])
+    return len(pet_shop["pets"])
 
-def get_pets_by_breed(var_input, breed):
-    breed_count = []
+def get_pets_by_breed(pet_shop, breed):
+    found_pets = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            found_pets.append(pet)
+    return found_pets
 
-    for pet in var_input["pets"]:
-        if pet ["breed"] == breed:
-            breed_count.append(breed)
-    return breed_count
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+    return None
 
-def find_pet_by_name(var_input, name_input): 
-    for pet in var_input["pets"]: 
-        if pet["name"] == None: 
-          return None 
+def remove_pet_by_name(pet_shop, pet_name):
+    pet = find_pet_by_name(pet_shop, pet_name)
+    if pet != None:
+        pet_shop["pets"].remove(pet)
 
-        elif pet ["name"] == name_input: 
-             return pet
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
 
-def remove_pet_by_name(var_input, pet_name_string):
-    pet_name_string == ("Arthur"):
-    for pet in var_input["pets"]:
-        if pet_name_string == ("Arthur"):
-           return pet.pop("Arthur")
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, amount):
+    customer["cash"] -= amount
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, pet):
+    customer["pets"].append(pet)
+
+def customer_can_afford_pet(customer, pet):
+    return customer["cash"] >= pet["price"]
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet and customer_can_afford_pet(customer, pet):
+        remove_customer_cash(customer, pet["price"])
+        remove_pet_by_name(pet_shop, pet["name"])
+        add_pet_to_customer(customer, pet)
+        add_or_remove_cash(pet_shop, pet["price"])
+        increase_pets_sold(pet_shop, 1)
